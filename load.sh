ab0639449d6edf1217cfef25069544d9e82e4312
Mark Manness <mark.manness@workiva.com>
2015-08-06 05:04:03 +0000

    Add memload

--- load.sh
+++ load.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+load_type=cpu
+if [ -n "$LOAD_TYPE" ]; then
+   load_type=$LOAD_TYPE
+fi
+
+# CPU args
 workload_arg=
 if [ -n "$WORKLOAD_TYPE" ]; then
    workload_arg="-w $WORKLOAD_TYPE"
@@ -20,21 +26,46 @@ if [ -n "$SEED" ]; then
    seed_arg="-s $SEED"
 fi
 
-debug_arg=
-if [ -n "$DEBUG" ]; then
-   debug_arg=-d
-fi
-
 threads=1
 if [ -n "$THREADS" ]; then
    threads=$THREADS
 fi
 
-i=0
-while [ $i -lt $threads ]; do
-   /usr/local/bin/load $workload_arg $arrival_arg $count_arg $seed_arg $debug_arg &
-   i=$(($i + 1))
-done
+# Memory args
+mem_start_arg=
+if [ -n "$MEM_START" ]; then
+   mem_start_arg="-s $MEM_START"
+fi
 
-wait
+mem_inc_arg=
+if [ -n "$MEM_INC" ]; then
+   mem_inc_arg="-i $MEM_INC"
+fi
+
+mem_interval_arg=
+if [ -n "$MEM_INTERVAL" ]; then
+   mem_interval_arg="-n $MEM_INTERVAL"
+fi
+
+# Debug arg
+debug_arg=
+if [ -n "$DEBUG" ]; then
+   debug_arg=-d
+fi
+
+if [ "$load_type" = "cpu" ]; then
+   i=0
+   while [ $i -lt $threads ]; do
+      /usr/local/bin/load $workload_arg $arrival_arg $count_arg $seed_arg $debug_arg &
+      i=$(($i + 1))
+   done
+   wait
+elif [ "$load_type" = "mem" ]; then
+   /usr/local/bin/memload $mem_starg_arg $mem_inc_arg $mem_interval_arg $debug 
+else
+   echo "Error: Unknown load_type '$load_type'"
+   exit 1
+fi
+
 echo "Done"
+exit 0
---
+++ memload.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <time.h>
+
+struct CONF {
+   int start; /* Interarrival rate for each transaction per second */
+   int increment; /* Number of transactions to exectue */
+   int interval; /* Number of transactions to exectue */
+   int debug; /* Debug mode */
+};
+
+void print_usage(char *basename) {
+   fprintf(stderr, "%s: [-s <start size >] [-i <increment size >]\n", basename);
+   fprintf(stderr, "   [-n <sleep interval>] [-d] [-h]\n");
+   fprintf(stderr, "   start size: in megabytes (default 100)\n");
+   fprintf(stderr, "   increment size: in megabytes (default 0)\n");
+   fprintf(stderr, "   sleep interval: in seconds (default 60)\n");
+   fprintf(stderr, "   -d: debug mode, shows values for each step\n");
+   fprintf(stderr, "   -h: print usage\n");
+}
+
+void parse_args(int argc, char **argv, struct CONF *confp) {
+   int opt;
+   confp->start = 100; /* Megabytes */
+   confp->increment = 0; /* Megabytes */
+   confp->interval = 60; /* Seconds */
+   confp->debug = 0;
+
+   while ( (opt = getopt(argc, argv, "hds:i:n:")) != -1 ) {
+      switch (opt) {
+      case 'h':
+         print_usage(argv[0]);
+         exit(0);
+         break;
+      case 's':
+         confp->start = atof(optarg);
+         break;
+      case 'i':
+         confp->increment = atoi(optarg);
+         break;
+      case 'n':
+         confp->interval = atoi(optarg);
+         break;
+      case 'd':
+         confp->debug = 1;
+         break;
+      default:
+         print_usage(argv[0]);
+         exit(1);
+      }
+   }
+}
+
+void run_load(struct CONF *confp) {
+   void *mem = malloc(confp->start * 1024 * 1024);
+   if ( confp->debug ) {
+      fprintf(stdout, "Starting malloc = %d MB\n", confp->start);
+      fprintf(stdout, "Sleeping %d seconds\n", confp->interval);
+   }
+
+   sleep(confp->interval);
+
+   for ( ; ; ) {
+      if ( confp->debug ) {
+         fprintf(stdout, "Allocating %d more MB\n", confp->increment);
+      }
+      mem = malloc(confp->increment * 1024 * 1024);
+
+      if ( confp->debug ) {
+         fprintf(stdout, "Sleeping %d seconds\n", confp->interval);
+      }
+
+      sleep(confp->interval);
+
+   }
+}
+
+int main(int argc, char **argv) {
+   struct CONF conf;
+   parse_args(argc, argv, &conf);
+
+   run_load(&conf);
+}
