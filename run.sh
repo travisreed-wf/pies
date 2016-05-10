c12434a5f24e454b230ab00dc4615d1ab412e91e
Mark Manness <mark.manness@workiva.com>
2016-05-10 19:48:39 +0000

    Use correct conf file

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" x="$http_x_forwarded_proto"'
 
 if [ -n "$LOG_FORMAT" ]; then
-   ex test.conf <<EOF
+   ex /etc/nginx/nginx.conf <<EOF
 /access_log/ i
 	log_format custom '$LOG_FORMAT';
 .
