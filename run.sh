d4883d24be21fd72c219ad08333ae45df8259f01
Mark Manness <mark.manness@workiva.com>
2016-05-10 16:10:39 +0000

    Remove debugging

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" x="$http_x_forwarded_proto"'
+# LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" x="$http_x_forwarded_proto"'
 
 if [ -n "$LOG_FORMAT" ]; then
    ex test.conf <<EOF
@@ -11,7 +11,5 @@ x
 EOF
    
 fi
-
-exit 0
 
 exec /usr/sbin/nginx
