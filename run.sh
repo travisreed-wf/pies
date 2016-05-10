68ae142327056e245d3064802740634ceb3d4990
Mark Manness <mark.manness@workiva.com>
2016-05-10 20:45:42 +0000

    Add log_format name to access line

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" x="$http_x_forwarded_proto"'
+CONF_FILE=/etc/nginx/nginx.conf
 
 if [ -n "$LOG_FORMAT" ]; then
-   ex /etc/nginx/nginx.conf <<EOF
+   ex $CONF_FILE <<EOF
 /access_log/ i
 	log_format custom '$LOG_FORMAT';
 .
+/access_log/ s/;/ custom;/
 x
 EOF
    
