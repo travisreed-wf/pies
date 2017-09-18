063bb90dda9f17221ead1ecdf0c23c125e630009
Mark.Manness <markmanness@LS2-00236.local>
2017-09-18 13:26:28 -0500

    Parameterize service prefix

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # LOG_FORMAT='$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" x="$http_x_forwarded_proto"'
 CONF_FILE=/etc/nginx/nginx.conf
 INDEX_FILE=/usr/share/nginx/html/index.html
+SERVER_FILE=/etc/nginx/sites-available/server
 
 if [ -n "$LOG_FORMAT" ]; then
    ex $CONF_FILE <<EOF
@@ -30,5 +31,11 @@ $BODY_TEXT
 .
 x
 EOF
+
+if [ -z "$HARBOUR_SERVICE_PREFIX" ]; then
+   export HARBOUR_SERVICE_PREFIX=/service/foo/
+fi
+
+sed -i -e "s|HARBOUR_SERVICE_PREFIX|${HARBOUR_SERVICE_PREFIX}|" $SERVER_FILE
 
 exec /usr/sbin/nginx
