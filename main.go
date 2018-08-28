55f0477322f23bf214aa4f3c62d9bb012d88ada5
Mark Manness <mark.manness@webfilings.com>
2018-08-28 16:47:49 -0500

    Update to have exponential log production rate

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 	"io/ioutil"
+	"math/rand"
     "time"
 
 	log "github.com/Sirupsen/logrus"
@@ -11,15 +14,55 @@ import (
     "github.com/rcrowley/go-metrics"
 )
 
+var logRate float64
+var telemetryInterval time.Duration
+var seed int64
+var randGen *rand.Rand
+
+func parseArgs() {
+	var t float64
+	flag.Float64Var(&logRate, "log-rate", 1.0, "Arrival rate for logs per second")
+	flag.Float64Var(&t, "telemetry-interval", 5.0, "Interval between telemetry emits")
+	flag.Int64Var(&seed, "seed", time.Now().UnixNano(), "Random number seed (defaults to current nanoseconds)")
+
+	flag.Parse()
+
+	telemetryInterval = time.Duration(int64(t * float64(time.Second)))
+}
+
+func calcSleepTime(arrivalRate float64) time.Duration {
+   var sleepval float64
+   sleepval = randGen.ExpFloat64() / arrivalRate
+   // fmt.Printf("calc sleepval = %f\n", sleepval)
+   duration := time.Duration(int64(sleepval * float64(time.Second)))
+   // fmt.Printf("duration_in = %T %v\n", duration, duration)
+   return duration
+}
+
 func main() {
-	// Add arg parse
+	parseArgs()
+	// fmt.Printf("Log Rate = %f\n", logRate)
+	// fmt.Printf("Telemetry Interval = %f\n", telemetryInterval)
+	// fmt.Printf("Seed = %d\n", seed)
+
+	randGen = rand.New(rand.NewSource(seed)) 
+
+	var sleepTime time.Duration
+	// var totalTime time.Duration
+	// totalTime = 0.0
+	// for i := 0; i < 20; i++ {
+	// 	sleepTime = calcSleepTime(logRate)
+	// 	fmt.Printf("sleepTime = %v\n", sleepTime)
+	// 	totalTime += sleepTime
+	// }
+	// fmt.Printf("totalTime = %v, average = %v\n", totalTime, totalTime / 20.0)
 
     meter := metrics.NewMeter()
     metrics.Register("quux", meter)
     meter.Mark(47)
 
     config := telemetry.GetDefaultConfig()
-	config.Interval = 5 * time.Second
+	config.Interval = telemetryInterval
     reporter, err := telemetry.NewReporterWithConfig(config)
     // current reporter constructors will always return a nil error.
     // The error is returned to allow for future changes without
@@ -34,13 +77,14 @@ func main() {
     log.AddHook(hook)
     log.SetOutput(ioutil.Discard) // prevent duplicate logs
 
-	log.WithFields(log.Fields{
-		"correlationId": "abc123",
-		"bar": 100,
-		"foo": "yo",
-	}).Info("I am a walrus")
+	for true {
+		log.WithFields(log.Fields{
+			"correlationId": "abc123",
+			"logRate": fmt.Sprintf("%f", logRate),
+			"foo": "bar",
+		}).Info("I am a happy walrus")
+		sleepTime = calcSleepTime(logRate)
+		time.Sleep(sleepTime)
+	}
 
-	log.Info("Sleeping")
-
-	time.Sleep(60 * time.Second)
 }
